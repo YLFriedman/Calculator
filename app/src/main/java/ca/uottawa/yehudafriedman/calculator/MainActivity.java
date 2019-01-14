debc902218ce965db47c0e598f91f81b4d291832
YLFriedman <37879744+YLFriedman@users.noreply.github.com>
2019-01-14 12:30:54 -0500

    Calculator brought up to full functionality

--- app/src/main/java/ca/uottawa/yehudafriedman/calculator/MainActivity.java
+++ app/src/main/java/ca/uottawa/yehudafriedman/calculator/MainActivity.java
@@ -4,19 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static StringBuilder storedExpression = new StringBuilder();
     private static StringBuilder displayExpression = new StringBuilder();
-    private static Value storedValue = null;
     private static Mode mode = Mode.STANDARD;
     private static TextView displayField;
     private int backspaceCounter = 0;
 
     private enum Mode{
-        STANDARD, ON_ERROR, RESULT, EXTEND_RESULT
+        STANDARD, ON_ERROR, RESULT, EXTEND_SCIENTIFIC
     }
 
     @Override
@@ -24,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayField = findViewById(R.id.textView);
+
+        ImageButton reset = findViewById(R.id.imageButton);
+        View.OnLongClickListener listener = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                clearExpression();
+                mode = Mode.STANDARD;
+                return true;
+            }
+        };
+        reset.setOnLongClickListener(listener);
 
     }
 
@@ -44,19 +54,27 @@ public class MainActivity extends AppCompatActivity {
                 return;
 
             case RESULT:
-                storedValue = null;
                 mode = Mode.STANDARD;
-                clearExpressions();
+                clearExpression();
                 displayExpression.append(bttnText);
                 displayField.setText(displayExpression.toString());
                 return;
 
-            case EXTEND_RESULT:
-                storedExpression.append(bttnText);
-                displayExpression.append(bttnText);
-                displayField.setText(displayExpression.toString());
-                backspaceCounter++;
-                return;
+            case EXTEND_SCIENTIFIC:
+                if (backspaceCounter > 0) {
+                    displayExpression.append(bttnText);
+                    displayField.setText(displayExpression.toString());
+                    backspaceCounter++;
+                    return;
+                }
+                else {
+                    mode = Mode.STANDARD;
+                    clearExpression();
+                    displayExpression.append(bttnText);
+                    displayField.setText(displayExpression.toString());
+                    return;
+                }
+
 
         }
 
@@ -78,14 +96,16 @@ public class MainActivity extends AppCompatActivity {
                 mode = Mode.STANDARD;
                 break;
 
-            case EXTEND_RESULT:
-                storedExpression.append(operator);
-
             case RESULT:
                 displayExpression.append(operator);
                 displayField.setText(displayExpression.toString());
+                mode = Mode.STANDARD;
+                break;
+
+            case EXTEND_SCIENTIFIC:
+                displayExpression.append(operator);
+                displayField.setText(displayExpression.toString());
                 backspaceCounter++;
-                mode = Mode.EXTEND_RESULT;
                 break;
 
         }
@@ -93,33 +113,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void onEqualsClick(View view) {
         String expression;
-        if(mode == Mode.EXTEND_RESULT) {
-            expression = storedExpression.toString();
-        }
-        else{
-            expression = displayExpression.toString();
-        }
-
-        clearExpressions();
+        expression = displayExpression.toString();
+        clearExpression();
 
         switch(ExpressionValidator.isValid(expression)) {
             case BAD_SYNTAX:
+
                 mode = Mode.ON_ERROR;
                 displayField.setText(getString(R.string.badExpressionMessage));
                 return;
 
             case DIVISION_BY_ZERO:
+
                 mode = Mode.ON_ERROR;
                 displayField.setText(getString(R.string.zeroDivisionError));
                 return;
 
             case VALID:
                 Value result = ExpressionValidator.processAndSolve(expression);
-                storedValue = result;
                 displayExpression.append(result.toString());
-                storedExpression.append(result.getValue().toString(true));
                 displayField.setText(displayExpression.toString());
-                mode = Mode.RESULT;
+                backspaceCounter = 0;
+                if(displayExpression.toString().matches(ExpressionValidator.containsScientific)) {
+                    mode = Mode.EXTEND_SCIENTIFIC;
+                }
+                else{
+                    mode = Mode.RESULT;
+                }
                 return;
         }
 
@@ -129,32 +149,97 @@ public class MainActivity extends AppCompatActivity {
     public void onDeleteClick(View view) {
 
         switch(mode) {
+            case EXTEND_SCIENTIFIC:
+                if(backspaceCounter == 0) {
+                    clearExpression();
+                    mode = Mode.STANDARD;
+                    return;
+                }
+                else if(displayExpression.toString().matches(ExpressionValidator.threeLetterSpecial)) {
+                    backspaceCounter = backspaceCounter - 4;
+                }
+                else if(displayExpression.toString().matches(ExpressionValidator.fourLetterSpecial)) {
+                    backspaceCounter = backspaceCounter - 5;
+                }
+                else{
+                    backspaceCounter--;
+                }
+
             case STANDARD:
-                displayExpression.deleteCharAt(displayExpression.length()-1);
-                displayField.setText(displayExpression.toString());
+                if(displayExpression.toString().matches(ExpressionValidator.threeLetterSpecial)) {
+                    displayExpression.delete(displayExpression.length()-4, displayExpression.length());
+                    displayField.setText(displayExpression.toString());
+                    return;
+                }
+                else if(displayExpression.toString().matches(ExpressionValidator.fourLetterSpecial)) {
+                    displayExpression.delete(displayExpression.length()-5, displayExpression.length());
+                    displayField.setText(displayExpression.toString());
+                    return;
+                }
+                else if(displayExpression.length() >= 1 ) {
+                    displayExpression.deleteCharAt(displayExpression.length()-1);
+                    displayField.setText(displayExpression.toString());
+                }
                 break;
 
             case ON_ERROR:
-                displayField.setText("");
+                clearExpression();
                 mode = Mode.STANDARD;
                 break;
 
             case RESULT:
-                displayField.setText("");
-                clearExpressions();
+                clearExpression();
                 mode = Mode.STANDARD;
                 break;
 
-            case EXTEND_RESULT:
-                displayExpression.deleteCharAt(displayExpression.length()-1);
-                storedExpression.deleteCharAt(displayExpression.length()-1);
-                backspaceCounter--;
-                break;
         }
     }
 
-    public static void clearExpressions() {
+    public void onAdvancedOperatorClick(View view) {
+        Button button = (Button) view;
+        String operator = button.getText().toString();
+
+        switch(mode) {
+            case EXTEND_SCIENTIFIC:
+                if(backspaceCounter == 0){
+                    clearExpression();
+                    displayExpression.append(operator + '(');
+                    displayField.setText(displayExpression.toString());
+                    mode = Mode.STANDARD;
+                    break;
+                }
+                else {
+                    displayExpression.append(operator + '(');
+                    displayField.setText(displayExpression.toString());
+                    if(operator.charAt(0) == 'A') {
+                        backspaceCounter += 5;
+                    }
+                    else {
+                        backspaceCounter += 4;
+                    }
+                    break;
+                }
+
+            case ON_ERROR:
+
+            case RESULT:
+                clearExpression();
+                displayExpression.append(operator + '(');
+                displayField.setText(displayExpression.toString());
+                mode = Mode.STANDARD;
+                break;
+
+            case STANDARD:
+                displayExpression.append(operator + '(');
+                displayField.setText(displayExpression.toString());
+                break;
+
+
+        }
+    }
+
+    private static void clearExpression() {
         displayExpression.delete(0, displayExpression.length());
-        storedExpression.delete(0, storedExpression.length());
+        displayField.setText("");
     }
 }
